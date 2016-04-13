3fe89f62b4c7a84ad8ceb55fa900f84064ae7258
Gavrilova <gavrilova.irina@gmail.com>
2016-04-12 23:41:03 -0500

    Method added to the class Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HomeWork2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HomeWork2.java
@@ -12,11 +12,7 @@ public class HomeWork2 {
 
     System.out.println("First  point: " + "(" + p1.x + ", " + p1.y + ")");
     System.out.println("Second point: " + "(" + p2.x + ", " + p2.y + ")");
-    System.out.println("Distance between points = " + distance(p1, p2));
+    System.out.println("Distance between points = " + p1.distance(p2));
   }
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + ((p2.y - p1.y) * (p2.y - p1.y)));
-
-  }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -13,5 +13,20 @@ public class Point {
 
   }
 
+  double dist(double x2, double y2) {
+
+    double x0 = x2 - this.x;
+    double y0 = y2 - this.y;
+    //System.out.println("x0 = " + x0 + " y0 = " + y0);
+    return Math.sqrt(x0 * x0 + y0 * y0);
+
+  }
+
+  double distance(Point p) {
+    return dist(p.x, p.y);
+
+  }
+
+
 }
 
