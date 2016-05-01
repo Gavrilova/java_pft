c4e790b8016b8c4c9c6ec3c962312dd856686c9b
Gavrilova <gavrilova.irina@gmail.com>
2016-04-30 23:18:49 -0500

    HomeWork9 Final

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -21,7 +21,7 @@ public class ContactDeletionTests extends TestBase {
         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
       }
       app.getNavigationHelper().gotoAddNewContactPage();
-      app.getContactHelper().createContact(new ContactData("Ira", "Aleksandrovna", "Gavrilova", "test4", "myNickname", "Peregrine Falcon Dr.", null, "123-456 7890", "234-567 8901", "345-678 9012", "5647", "gavrilova.irina@gmail.com", "http://www.zello.com/", "test1"), true);
+      app.getContactHelper().createContact(new ContactData("Ira", "Aleksandrovna", "Gavrilova", "test4", "myNickname", "Peregrine Falcon Dr.", null, "123-456 7890", "234-567 8901", "345-678 9012", "5647", "gavrilova.irina@gmail.com", "http://www.zello.com/", "test1"));
       app.getNavigationHelper().gotoHomePage();
     }
     app.getContactHelper().selectContact();
