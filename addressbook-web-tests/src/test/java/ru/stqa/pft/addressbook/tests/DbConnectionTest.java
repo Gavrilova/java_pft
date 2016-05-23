992de77569358322a3b813b35a85add2661ff3ec
Gavrilova <gavrilova.irina@gmail.com>
2016-05-23 13:29:18 -0500

    Added contact information form DbConnectionTest class

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
@@ -25,11 +27,34 @@ public class DbConnectionTest {
         groups.add(new GroupData().withId(rs.getInt("group_id")).withName((rs.getString("group_name")))
                 .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
       }
+
+      ResultSet rsContacts = st.executeQuery("select id, firstname, middlename, lastname, nickname," +
+              "company, title, address, home, mobile, work, fax, email, email2, email3, homepage from addressbook");
+      Contacts contacts = new Contacts();
+      while (rsContacts.next()) {
+        contacts.add(new ContactData().withId(rsContacts.getInt("id"))
+                .withFirstname(rsContacts.getString("firstname"))
+                .withMiddlename(rsContacts.getString("middlename"))
+                .withLastname(rsContacts.getString("lastname"))
+                .withNickname(rsContacts.getString("nickname"))
+                .withCompany(rsContacts.getString("company"))
+                .withTitle(rsContacts.getString("title"))
+                .withHomePhone(rsContacts.getString("home"))
+                .withMobilePhone(rsContacts.getString("mobile"))
+                .withWorkPhone(rsContacts.getString("work"))
+                .withFax(rsContacts.getString("fax"))
+                .withEmail(rsContacts.getString("email"))
+                .withEmail2(rsContacts.getString("email2"))
+                .withEmail3(rsContacts.getString("email3"))
+                .withHomepage(rsContacts.getString("homepage")));
+      }
+      rsContacts.close();
       rs.close();
       st.close();
       conn.close();
 
       System.out.println(groups);
+      System.out.println(contacts);
 
     } catch (SQLException ex) {
       // handle any errors
